4b3ec7c9cc38f71d32656c472a4072a924849eef
Alejandro <alequinteroz75@gmail.com>
2022-07-19 13:09:09 -0600

    Primer commit, en proceso la Navbar
    donde se colocara la informacion es en el
    RouterNav para que pueda colocar las paginas en
    esta zona

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { 
-  BrowserRouter,
-  Routes,
-  Route,
- } from 'react-router-dom';
-import App from './App'
-import 'src\styles\_main.scss';
+import ReactDOM from 'react-dom/client' 
+import { Navbar } from './components/Navbar/Navbar';
+import "./styles/_main.scss"; 
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}></Route>
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <Navbar />
+  </React.StrictMode>
 )
