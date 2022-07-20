68a276b6bdf558bc5b2686bcbb30f195ab1f7c20
DannyST89 <dsdevelopercr@outlook.com>
2022-07-19 21:53:05 -0600

    Carousel
    Se agrega el carousel del Index
    Faltan algunas modificaciones en los estilos.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client' 
-import { Navbar } from './components/Navbar/Navbar';
-import "./styles/_main.scss"; 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+import { Navbar } from "./components/Navbar/Navbar";
+import "./styles/_main.scss";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Navbar />
   </React.StrictMode>
-)
+);
