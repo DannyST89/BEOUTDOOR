2638bc47ee997d245e1965a4bf74fdbbf6aded9e
Alejandro <alequinteroz75@gmail.com>
2022-07-20 10:36:05 -0600

    Cambio en navbar, y en importaciones
    se cambiaron donde se ubican las rutas,
    se cambio a ContentPage, ahi es donde se colocara
    las paginas, o las rutas mejor dicho

--- src/components/Navbar/components/ContentPage.tsx
+++ src/components/Navbar/components/ContentPage.tsx
@@ -3,7 +3,7 @@ import {
     Route, 
 } from 'react-router-dom';
 import { Index } from '../../../pages/index/Index';
-export const RouterNav = () => {
+export const ContentPage = () => {
   return (
     <Routes>
        <Route path="/" element={<Index />} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,27 @@
 
 import React from 'react'
-import ReactDOM from 'react-dom/client' 
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
+import { Footer } from './components/Footer/Footer';
+import { ContentPage } from './components/Navbar/components/ContentPage';
 import { Navbar } from './components/Navbar/Navbar';
-import "./styles/_main.scss";  
+import "./styles/_main.scss";
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Navbar />
+    <BrowserRouter>
+
+      {/* Navegacion */}
+      <Navbar />
+
+      {/* Contenido */}
+      <ContentPage />
+
+      {/* Footer */}
+      <Footer />
+
+    </BrowserRouter>
   </React.StrictMode>
 );
 
