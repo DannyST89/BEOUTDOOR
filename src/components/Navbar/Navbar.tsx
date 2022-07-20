21c62a2b79899269599f9f73a5a7e601e3c45537
Alejandro <alequinteroz75@gmail.com>
2022-07-19 20:36:04 -0600

    Cambios en la navegacion
    se cambio el color, se cambio a fixed
    se manejo el espacio de cada dato

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,73 +1,82 @@
 import {
-    BrowserRouter, 
-    NavLink,
-    Router
+    BrowserRouter,
+    NavLink
 } from 'react-router-dom';
 import { RouterNav } from './components/RouterNav';
-
+import { Nav } from 'reactstrap';
 export const Navbar = () => {
 
-
     return (
-        <div>
+        <div className=''>
             <BrowserRouter>
-                <nav className='navbar navbar-expand-lg bg-light'>
-                    <div className='container-fluid '>
+                <Nav className='navbar navbar-expand-lg backgroundNav fixed-top '>
+                    <div className='container-fluid container justify-content-between'>
+                        
+                        <div className='logo'>
+                            <h4>LOGO</h4>
+                        </div>
 
-                        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                        <button className="navbar-toggler " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                             <span className="navbar-toggler-icon"></span>
                         </button>
 
-                        <div className="offcanvas offcanvas-end" tabIndex={-1} id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                            <div className="offcanvas-header">
+                        <div className="offcanvas offcanvas-end " tabIndex={-1} id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                            <div className="offcanvas-header ">
                                 <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
                                 <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                             </div>
- 
+
                             {/* informacion que se mostrara en celular */}
-                            <div className="offcanvas-body">
-                                <ul className='navbar-nav'>
-                                    <li data-bs-dismiss="offcanvas" className='nav-item'><NavLink className="nav-link" to="/">Inicio</NavLink></li>
-                                    <li className="nav-item dropdown">
-                                        <a  className="nav-link dropdown-toggle" href="adventures" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            Adventures
-                                        </a>
-                                        <ul className='dropdown-menu'>
-                                            <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/rafting">Rafting</NavLink> </li>
-                                            <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/safari">Safari</NavLink> </li>
-                                        </ul>
-                                    </li>
-                                    <li className='nav-item dropdown'>
-                                        <a className="nav-link dropdown-toggle" href="hikings" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            Hikings
-                                        </a>
-                                        <ul className='dropdown-menu'>
-                                            <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/rioCeleste">Rio Celeste</NavLink> </li>
-                                            <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/arenalVolcano">Arenal Volcano</NavLink> </li>
-                                            <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/laFortunaWaterfall">La Fortuna Waterfall</NavLink> </li>
-                                            <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/hangingBridges">Hanging Bridges</NavLink> </li>
-                                            <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/combinationTours">Combination Tours</NavLink> </li>
-                                        </ul>
-                                    </li>
-                                    <li className="nav-item dropdown">
-                                        <a className="nav-link dropdown-toggle" href="about" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            About
-                                        </a>
+                            <div className="offcanvas-body ">
+                                <div className='logo'>
+                                     
+                                </div>
+                                <div className='offcanvas-body'>
+                                    <ul className='navbar-nav '>
+                                        <li data-bs-dismiss="offcanvas" className='nav-item'><NavLink className="nav-link" to="/">Inicio</NavLink></li>
+                                        <li className="nav-item dropdown" >
+                                            <a className="nav-link dropdown-toggle" href="adventures" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                Adventures
+                                            </a>
+                                            <ul className='dropdown-menu'>
+                                                <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/rafting">Rafting</NavLink> </li>
+                                                <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/safari">Safari</NavLink> </li>
+                                            </ul>
+                                        </li>
+                                        <li className='nav-item dropdown'>
+                                            <a className="nav-link dropdown-toggle" href="hikings" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                Hikings
+                                            </a>
+                                            <ul className='dropdown-menu'>
+                                                <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/rioCeleste">Rio Celeste</NavLink> </li>
+                                                <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/arenalVolcano">Arenal Volcano</NavLink> </li>
+                                                <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/laFortunaWaterfall">La Fortuna Waterfall</NavLink> </li>
+                                                <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/hangingBridges">Hanging Bridges</NavLink> </li>
+                                                <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/combinationTours">Combination Tours</NavLink> </li>
+                                            </ul>
+                                        </li>
+                                        <li className="nav-item dropdown">
+                                            <a className="nav-link dropdown-toggle" href="about" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                About
+                                            </a>
 
-                                        <ul className='dropdown-menu'>
-                                            <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/aboutTheCompany">About the Company</NavLink> </li>
-                                            <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/aboutTheFortuna">About the Fortuna</NavLink> </li>
-                                        </ul>
-                                    </li>
-                                    <li data-bs-dismiss="offcanvas" className='nav-item'><NavLink className="nav-link" to="/contactUs">Contact us</NavLink> </li>
-                                </ul>
-                            </div> 
-                        </div> 
+                                            <ul className='dropdown-menu'>
+                                                <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/aboutTheCompany">About the Company</NavLink> </li>
+                                                <li data-bs-dismiss="offcanvas"><NavLink className="dropdown-item" to="/aboutTheFortuna">About the Fortuna</NavLink> </li>
+                                            </ul>
+                                        </li>
+                                        <li data-bs-dismiss="offcanvas" className='nav-item'><NavLink className="nav-link" to="/contactUs">Contact us</NavLink> </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </nav>
+                </Nav>
 
-                {/* componente para mostrar las las paginas */}
-                <RouterNav />
+                <div className='paginasContainer container'>
+                    {/* componente para mostrar las las paginas */}
+                    <RouterNav />
+                </div>
 
             </BrowserRouter>
         </div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client' 
 import { Navbar } from './components/Navbar/Navbar';
 import "./styles/_main.scss"; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Navbar />
-  </React.StrictMode>
+  // {/* </React.StrictMode> */}
 )
