70c6e4cfe2ed8102a2dbb3d04a7f55d9abeb7eb2
Alejandro <alequinteroz75@gmail.com>
2022-07-25 13:29:20 -0600

    En proceso

--- src/assets/images/Images.ts
+++ src/assets/images/Images.ts
@@ -7,4 +7,7 @@ export { default as safari } from '../../assets/images/safari.jpg'
 export { default as rafting1 } from '../../assets/images/rafting1.jpg'
 export { default as rafting2 } from '../../assets/images/rafting2.jpg'
 export { default as rana } from '../../assets/images/[000072].jpg'
+export { default as hikings } from '../../assets/images/hikings.jpg'
+export { default as hikings1 } from '../../assets/images/hikings1.jpg'
+export { default as hikings2 } from '../../assets/images/hikings2.jpg'
 
