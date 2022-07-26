85bdb61a9c07fa426e1c17c3ce2e24fd606f18a6
Alejandro <alequinteroz75@gmail.com>
2022-07-25 20:24:39 -0600

    Gallery en proceso

--- src/pages/index/components/Gallery.tsx
+++ src/pages/index/components/Gallery.tsx
@@ -4,28 +4,55 @@ export const Gallery = () => {
 
     const [images, setImages] = useState(0);
 
+    const [active, setActive] = useState("");
+
+    //=========================================================
+
+    // Get the size of the device screen
+    var screenWidth = screen.width;
+    var screenHeight = screen.height;
+
+    // Get the browser window size
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+
+    // Get the size of the entire webpage
+    const pageWidth = document.documentElement.scrollWidth;
+    const pageHeight = document.documentElement.scrollHeight;
+
+    console.log("screenWidth: " + screenWidth);
+    console.log("screenHeight: " + screenHeight);
+    console.log("windowWidth: " + windowWidth);
+    console.log("windowHeight: " + windowHeight);
+
 
 
     return (
         <div>
-
             <nav className="navbar navbar-expand-lg mt-5">
-                <div className="container-fluid">  
+                <div className="container-fluid">
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav navbarGallery">
                             <li className="nav-item" onClick={() => setImages(0)}>
-                                <a className="nav-link" onClick={(e) => e.preventDefault()} href="">All</a>
+                                <a
+                                    className={active === 'all' ? 'aGallery nav-link' : 'nav-link'}
+                                    onClick={(e) => { e.preventDefault(); setActive('all') }}
+                                >All</a>
                             </li>
-                            <li className="nav-item aGallery" onClick={() => setImages(1)}>
-                                <a className="nav-link aGallery" onClick={(e) => e.preventDefault()} href="">Snake</a>
+                            <li className="nav-item" onClick={() => setImages(1)}>
+                                <a
+                                    className={active === 'snake' ? 'aGallery nav-link' : 'nav-link'}
+                                    onClick={(e) => { e.preventDefault(); setActive('snake') }}
+                                >Snake</a>
                             </li>
                             <li className="nav-item " onClick={() => setImages(2)}>
-                                <a className="nav-link aGallery" onClick={(e) => e.preventDefault()} href="">Lizard</a>
+                                <a
+                                    className={active === 'lizard' ? 'aGallery nav-link' : 'nav-link'}
+                                    onClick={(e) => { e.preventDefault(); setActive('lizard') }}
+                                >Lizard</a>
                             </li>
                         </ul>
                     </div>
-
-
                 </div>
             </nav>
 
@@ -44,8 +71,6 @@ export const Gallery = () => {
                                     <div className="cardGallery" style={{ backgroundImage: "url('https://source.unsplash.com/random/4')" }}></div>
                                     <div className="cardGallery" style={{ backgroundImage: "url('https://source.unsplash.com/random/5')" }}></div>
                                     <div className="cardGallery" style={{ backgroundImage: "url('https://source.unsplash.com/random/6')" }}></div>
-
-
                                 </div>
                                 : null
                         }
@@ -74,12 +99,9 @@ export const Gallery = () => {
                                 </div>
                                 : null
                         }
-
                     </div>
                 </div>
             </section>
-
-
         </div>
     )
 }
