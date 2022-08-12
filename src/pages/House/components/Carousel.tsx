ea53632769ffdb282ccbad26f9d6c9e9ddead499
Alejandro <alequinteroz75@gmail.com>
2022-08-11 23:05:09 -0600

    proceso

--- src/pages/House/components/Carousel.tsx
+++ src/pages/House/components/Carousel.tsx
@@ -1,82 +1,24 @@
 import * as images from "../../../assets/images/Images";
 import Slider from "react-slick";
 import React, { useState } from "react";
-import { BiDollar } from "react-icons/bi";
 
-
-const sliderData = [
-    {
-        id: 1,
-        title: "Volcan Arenal Hike",
-        priceDetails: "Price per person",
-        price: "100",
-        dollarIcon: (
-            <BiDollar style={{ color: "#FFFFFF", fontSize: "1rem" }} />
-        ),
-        urlImage: images.volcanArenal01,
-        description: "description for the arenal volcan hike very short",
-    },
-    {
-        id: 2,
-        title: "Rio Celeste Hike",
-        priceDetails: "Price per person",
-        price: "100",
-        dollarIcon: (
-            <BiDollar style={{ color: "#FFFFFF", fontSize: "1rem" }} />
-        ),
-        urlImage: images.rioCeleste02,
-        description: "description for the arenal volcan hike very short",
-    },
-    {
-        id: 3,
-        title: "Hanging Bridges",
-        priceDetails: "Price per person",
-        price: "80",
-        dollarIcon: (
-            <BiDollar style={{ color: "#FFFFFF", fontSize: "1rem" }} />
-        ),
-        urlImage: images.hangingBridges,
-        description: "description for the arenal volcan hike very short",
-    },
-    {
-        id: 4,
-        title: "Fortuna Waterfall",
-        priceDetails: "Price per person",
-        price: "70",
-        dollarIcon: (
-            <BiDollar style={{ color: "#FFFFFF", fontSize: "1rem" }} />
-        ),
-        urlImage: images.rioCeleste01,
-        description: "description for the arenal volcan hike very short",
-    },
-    {
-        id: 5,
-        title: "Safari Float and Wild Life Arenal",
-        priceDetails: "Price per person",
-        price: "80",
-        dollarIcon: (
-            <BiDollar style={{ color: "#FFFFFF", fontSize: "1rem" }} />
-        ),
-        urlImage: images.safariFloatWildLifeArenal,
-        description: "description for the arenal volcan hike very short",
-    },
-    {
-        id: 6,
-        title: "Rafting",
-        priceDetails: "Price per person",
-        price: "80",
-        dollarIcon: (
-            <BiDollar style={{ color: "#FFFFFF", fontSize: "1rem" }} />
-        ),
-        urlImage: images.rafting2,
-        description: "description for the arenal volcan hike very short",
-    },
+const sliderData = [ 
+    images.house1,
+    //images.house2,
+    //images.house3,
+    images.house4,
+    //images.house5,
+    images.house6,
+    images.house7,
+    images.house8,
+    images.house9,
+    images.house10,
 ];
 
 export const CarouselHome = () => {
 
 
-    const [urlImage, setUrlImage] = useState(sliderData[0].urlImage);
+    const [urlImage, setUrlImage] = useState(sliderData[0]);
 
     const settings = {
         dots: false,
@@ -100,6 +42,7 @@ export const CarouselHome = () => {
                     slidesToShow: 2,
                     slidesToScroll: 2,
                     initialSlide: 2,
+                    dots: true,
                 },
             },
             {
@@ -107,6 +50,7 @@ export const CarouselHome = () => {
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
+                    dots: true,
                 },
             },
         ],
@@ -123,14 +67,13 @@ export const CarouselHome = () => {
             </div>
 
             <div className="">
-
-                <Slider {...settings} className='carouselHouse'>
+                <Slider {...settings} className='carouselHouseSlider'>
                     {
                         React.Children.toArray(
                             sliderData.map((item) => (
                                 <div className="">
-                                    <div onClick={() => { setUrlImage(item.urlImage) }} className="carouselHouse__imageSmall">
-                                        <img className="carouselHouse__imageSmall-img " src={item.urlImage} alt={item.title} />
+                                    <div onClick={() => { setUrlImage(item) }} className="carouselHouse__imageSmall">
+                                        <img className="carouselHouse__imageSmall-img " src={item} alt='Image House' />
                                     </div>
                                 </div>
                             ))
