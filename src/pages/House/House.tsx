76bbde6f46403ecb09195fdcfb5eaf5df9b07dae
DannyST89 <dsdevelopercr@outlook.com>
2022-08-12 23:29:24 -0600

    se arreglan los nombres de los archivos

--- src/pages/House/House.tsx
+++ src/pages/House/House.tsx
@@ -1,10 +1,13 @@
 import { Fragment } from "react"
 import DescriptionCard from "./components/DescriptionCard"
+import { CarouselHome } from "./components/CarouselHome";
+import { HouseAside } from "./components/HouseAside";
 
 
 export const House = () => {
   return (
-    <>
+    <>  <HouseAside/>
+        <CarouselHome />
         <DescriptionCard/>
     </>
   )
