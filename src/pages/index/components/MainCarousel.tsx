187013e6807e5773117a3e8c8d9ade3bceed06ee
DannyST89 <dsdevelopercr@outlook.com>
2022-09-05 18:36:55 -0600

    se elimina el componente scroll

--- src/pages/index/components/MainCarousel.tsx
+++ src/pages/index/components/MainCarousel.tsx
@@ -1,9 +1,7 @@
 import styles from "../../../styles/components/index/_MainCarousel.module.scss";
 import * as images from "../../../assets/images/Images";
 import video from "../../../assets/videos/video.mp4";
-
 import { useRef, useState } from "react";
-import { Scroll } from "../../../components/scroll/Scroll";
 
 export const MainCarousel = () => {
   
@@ -16,7 +14,6 @@ export const MainCarousel = () => {
         <h1 className={styles["content-title"]}>Be Outdoors with us</h1>
         <p className={styles["content-p"]}>Discover La Fortuna</p>
         <button className={styles["btn"]}> Get in Touch</button>
-        <Scroll />
       </div>
     </div>
   );
