7385dbf403022b785d51ceb2cd64d214107ddf69
DannyST89 <dsdevelopercr@outlook.com>
2022-09-12 17:38:52 -0600

    seccion de preguntas

---
+++ src/pages/index/components/FrequentlyAskedQuestions.tsx
@@ -0,0 +1,95 @@
+import styles from "../../../styles/components/index/_FrequentlyAskedQuestions.module.scss";
+export const FrequentlyAskedQuestions = () => {
+  return (
+    <div className={`container ${styles["questions__content"]}`}>
+      <div className={styles["questions__title"]}>
+        <h2>Frequently Asked Questions</h2>
+        <span></span>
+      </div>
+      <div className="accordion accordion-flush" id="accordionFlushExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="flush-headingOne">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseOne"
+              aria-expanded="false"
+              aria-controls="flush-collapseOne"
+            >
+              <strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, explicabo?.</strong> 
+            </button>
+          </h2>
+          <div
+            id="flush-collapseOne"
+            className="accordion-collapse collapse"
+            aria-labelledby="flush-headingOne"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className={`accordion-body ${styles["accordion__color"]}`}>
+              Placeholder content for this accordion, which is intended to
+              demonstrate the class. This is the
+              first item's accordion body.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="flush-headingTwo">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseTwo"
+              aria-expanded="false"
+              aria-controls="flush-collapseTwo"
+            >
+               <strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, explicabo?.</strong>
+            </button>
+          </h2>
+          <div
+            id="flush-collapseTwo"
+            className="accordion-collapse collapse"
+            aria-labelledby="flush-headingTwo"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className={`accordion-body ${styles["accordion__color"]}`}>
+              Placeholder content for this accordion, which is intended to
+              demonstrate the class. This is the
+              second item's accordion body. Let's imagine this being filled with
+              some actual content.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="flush-headingThree">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseThree"
+              aria-expanded="false"
+              aria-controls="flush-collapseThree"
+            >
+              <strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, explicabo?.</strong>
+            </button>
+          </h2>
+          <div
+            id="flush-collapseThree"
+            className="accordion-collapse collapse"
+            aria-labelledby="flush-headingThree"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className={`accordion-body ${styles["accordion__color"]}`}>
+              Placeholder content for this accordion, which is intended to
+              demonstrate the class. This is the
+              third item's accordion body. Nothing more exciting happening here
+              in terms of content, but just filling up the space to make it
+              look, at least at first glance, a bit more representative of how
+              this would look in a real-world application.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
