2ec1e2e860557060fb17eff445e466ae60733de8
DannyST89 <dsdevelopercr@outlook.com>
2022-09-12 14:19:28 -0600

    cambios en el footer Footer
    se quita la imagen del volcan

--- src/assets/images/Images.ts
+++ src/assets/images/Images.ts
@@ -1,7 +1,7 @@
 
 export { default as volcanArenal01 } from '../../assets/images/volcanArenal01.jpg'
 export { default as lagoArenal01 } from '../../assets/images/lagoArenal01.jpg'
-export { default as rioCeleste01 } from '../../assets/images/IMG-20220313-WA0058.jpg'
+export { default as rioCeleste01 } from '../../assets/images/rioCeleste01.jpg'
 export { default as rioCeleste02 } from '../../assets/images/IMG-20220313-WA0058.jpg'
 export { default as imageFooter } from '../../assets/images/20220604_091349.jpg'
 export { default as safari } from '../../assets/images/safari.jpg'
