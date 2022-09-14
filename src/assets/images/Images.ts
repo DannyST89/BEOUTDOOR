6cf11f3ea8cc5b88cd82d73d9cb1a9b829f73eed
DannyST89 <dsdevelopercr@outlook.com>
2022-09-14 16:00:14 -0600

    se ordenan imagenes

--- src/assets/images/Images.ts
+++ src/assets/images/Images.ts
@@ -1,5 +1,15 @@
 
-export { default as volcanArenal01 } from '../../assets/images/volcanArenal01.jpg'
+// Images tour volcan arenal
+export { default as volcanArenal01 } from '../../assets/images/Tour_Caminata_volcan/arenalHike01.jpg'
+export { default as volcanArenal02 } from '../../assets/images/Tour_Caminata_volcan/arenalHike02.jpg'
+export { default as volcanArenal03 } from '../../assets/images/Tour_Caminata_volcan/arenalHike03.jpg'
+export { default as volcanArenal04 } from '../../assets/images/Tour_Caminata_volcan/arenalHike04.jpg'
+export { default as volcanArenal05 } from '../../assets/images/Tour_Caminata_volcan/arenalHike05.jpg'
+export { default as volcanArenal06 } from '../../assets/images/Tour_Caminata_volcan/arenalHike06.jpg'
+export { default as volcanArenal07 } from '../../assets/images/Tour_Caminata_volcan/arenalHike07.jpg'
+export { default as volcanArenal08 } from '../../assets/images/Tour_Caminata_volcan/arenalHike08.jpg'
+
+// Images tour Rio Celeste
 export { default as lagoArenal01 } from '../../assets/images/lagoArenal01.jpg'
 export { default as rioCeleste01 } from '../../assets/images/rioCeleste01.jpg'
 export { default as rioCeleste02 } from '../../assets/images/IMG-20220313-WA0058.jpg'
@@ -8,11 +18,18 @@ export { default as safari } from '../../assets/images/safari.jpg'
 export { default as rafting1 } from '../../assets/images/rafting1.jpg'
 export { default as rafting2 } from '../../assets/images/rafting2.jpg'
 export { default as rana } from '../../assets/images/[000072].jpg'
+
+// Images tour Hikings
 export { default as hikings } from '../../assets/images/hikings.jpg'
 export { default as hikings1 } from '../../assets/images/hikings1.jpg'
 export { default as hikings2 } from '../../assets/images/hikings2.jpg'
 export { default as safariFloatWildLifeArenal } from '../../assets/images/20210624_102736.jpg'
+
+// Images tour hanging bridges
 export { default as hangingBridges} from '../../assets/images//20220324_153317.jpg'
+
+
+// Images Casa Sunrise
 export { default as house1} from '../../assets/images/house1.webp'
 export { default as house2} from '../../assets/images/house2.webp'
 export { default as house3} from '../../assets/images/house3.webp'
@@ -23,10 +40,16 @@ export { default as house7} from '../../assets/images/house7.webp'
 export { default as house8} from '../../assets/images/house8.webp'
 export { default as house9} from '../../assets/images/house9.webp'
 export { default as house10} from'../../assets/images/house10.webp'
+
+// Images tour tubing
 export { default as tubing1} from'../../assets/images/IMG_1147.jpeg'
 export { default as tubing2} from'../../assets/images/IMG_1171.jpeg'
 export { default as tubing3} from'../../assets/images/IMG_1456.jpeg'
 export { default as tubing4} from'../../assets/images/house10.webp'
+
+// Images gallery Index
 export { default as smilisca_phaeota} from'../../assets/images/smilisca_phaeota.jpg'
+
+// Images tour catarata la fortuna
 export { default as catarata_fortuna} from'../../assets/images/catarata_fortuna.jpeg'
 export { default as catarata_fortuna1} from'../../assets/images/20210417_103333.jpg'
